551b2ebd0dfe7b13c401fae88ee99718a854155f
utsavisama <82657781+utsavisama@users.noreply.github.com>
2022-01-04 18:02:21 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,10 +1,24 @@
 function setup(){
 canvas = createCanvas(410, 450)
-canvas.position(10, 100)
+canvas.position(1000, 100)
 video = createCapture(VIDEO)
-video.hide()
+video.size(300, 450)
+video.position(10, 100)
+
+var posenet = ml5.poseNet(video, modelLoaded)
+posenet.on('pose', gotResults)
+
 }
 
+
+function modelLoaded(){
+    console.log("POSENET IS LOADED!")
+}
 function draw(){
-image(video, 0,0,550,450)
+}
+
+function gotResults(results){
+if(results.length>0){
+    console.log(results)
+}
 }
